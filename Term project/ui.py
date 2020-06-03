ab047579e84f4fc13b6efca27ac8972087ea1a19
neltique <31910313+neltique@users.noreply.github.com>
2020-06-04 03:18:03 +0900

    image update
    image update

--- Term project/ui.py
+++ Term project/ui.py
@@ -1,51 +1,135 @@
 from tkinter import *
 from tkinter import font
+from tkinter import ttk
 
-width = 900
-height = 500
-def UI():
-    window = Tk()
-    window.title("테스트")
-    window.geometry('900x500')
-    window.configure(bg="skyblue")
-    window.resizable(True,True)
-    fontstyle = font.Font(window, size=8, weight='bold', family='Consolas')
+from io import BytesIO
+import urllib
+import urllib.request
+from PIL import Image,ImageTk
 
+WIDTH = 900
+HEIGHT = 450
 
-    listcanvas = Canvas(window, width = 300 , height = 430 ,bg="white" )
-    listcanvas.place(x=10,y=50)
-
-    secondCanvas=Canvas(window, width = 450 , height = 430 ,bg="white" )
-    secondCanvas.place(x=350, y=50)
-
-##########################################################################
-    firstTab = Canvas(window, width=60, height=70, bg="white")
-    firstTab.place(x=820, y=50)
-
-    secondTab = Canvas(window, width=60, height=70, bg="white")
-    secondTab.place(x=820, y=150)
-
-    thirdTab = Canvas(window, width=60, height=70, bg="white")
-    thirdTab.place(x=820, y=250)
-
-    forthTab = Canvas(window, width=60, height=70, bg="white")
-    forthTab.place(x=820, y=350)
-############################################################################
-#검색 entry:string을 받아 검색 button 누르면 string 읽어오기
-    filename = StringVar()
-    entry1 = Entry(window, width=15, textvariable=filename)
-    entry1.place(x=270, y=10)
-
-    filename2 = StringVar()
-    entry2 = Entry(window, width=15, textvariable=filename2)
-    entry2.place(x=400, y=10)
-
-#글씨
-    search = Button(text="검색",width = 3 ,height= 1 ,font=fontstyle ,bg="white",fg="black")
-    search.place(x=530,y=10)
+class UI():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("ReSCH")
+        self.window.geometry(str(WIDTH)+'x'+str(HEIGHT))
+        self.window.configure(bg="skyblue")
+        self.window.resizable(True,True)
+        self.fontstyle = font.Font(self.window, size=8, weight='bold', family='Consolas')
 
 
-    window.mainloop()
+        self.setupCombobox()
+        self.setupButton()
+        self.setupListbox()
+
+        self.window.option_add('*TCombobox*Listbox.font', self.combofont)
+
+
+
+
+
+        self.secondCanvas=Canvas(self.window, width = 450 , height = 380 ,bg="white" )
+        self.secondCanvas.place(x=350, y=50)
+
+########################################################################################################################
+
+        self.window.mainloop()
+
+    def setupListbox(self):
+        #self.listCanvas = Canvas(self.window, width = 300 , height = 385 ,bg="white" )
+        #self.listCanvas.place(x=10,y=50)
+
+        self.adressfont = font.Font(self.window, size=11, weight='bold', family='Consolas')
+        self.adressList = Listbox(self.window,selectmode='extended', height=20)
+        self.adressList['width'] = 40
+        self.adressList['font'] = self.adressfont
+        self.adressList['activestyle']='none'
+
+        ###########XML###########
+        self.adressList.insert(0, "경기도 시흥시 정왕동 1호")
+        self.adressList.insert(1, "경기도 시흥시 정왕동 2호")
+        self.adressList.insert(3, "경기도 시흥시 정왕동 3호")
+        self.adressList.insert(4, "경기도 시흥시 정왕동 4호")
+        self.adressList.insert(5, "경기도 시흥시 정왕동 5호")
+        self.adressList.insert(6, "경기도 시흥시 정왕동 6호")
+
+        self.adressList.place(x=10,y=50)
+
+
+# 버튼 설정
+    def setupButton(self):
+        # 검색 버튼
+        self.searchfont = font.Font(self.window, size=11, weight='bold', family='Consolas')
+        self.searchButton = Button(text="검색", width=5, height=1, font=self.searchfont, bg="white", fg="black",command=self.search)
+        self.searchButton.place(x=430, y=10)
+
+        # 취소 버튼
+
+        self.undoImg = Image.open("img/undo.png")
+        self.undoImg = self.undoImg.resize((25,25),Image.ANTIALIAS)
+        self.resizeUndoImg = ImageTk.PhotoImage(self.undoImg)
+
+        self.undoButton = Button(self.window, width=27, height=27, bg="white", command=self.undo)
+        self.undoButton["image"] = self.resizeUndoImg
+        self.undoButton.place(x=500, y=10)
+
+        # 정보 버튼
+        self.infoImg = Image.open("img/info.png")
+        self.infoImg = self.infoImg.resize((60,60), Image.ANTIALIAS)
+        self.resizeInfoImg = ImageTk.PhotoImage(self.infoImg)
+        self.infoTab = Button(self.window, width=60, height=60, bg="white")
+        self.infoTab["image"] = self.resizeInfoImg
+        self.infoTab.place(x=820, y=50)
+
+        # 지도 버튼
+        self.mapImg = Image.open("img/map.png")
+        self.mapImg = self.mapImg.resize((60,60), Image.ANTIALIAS)
+        self.resizeMapImg = ImageTk.PhotoImage(self.mapImg)
+        self.mapTab = Button(self.window, width=60, height=60, bg="white")
+        self.mapTab["image"] = self.resizeMapImg
+        self.mapTab.place(x=820, y=156)
+
+        # 메일 보내기 버튼
+        self.mailImg = Image.open("img/mail.png")
+        self.mailImg = self.mailImg.resize((60,60), Image.ANTIALIAS)
+        self.resizeMailImg = ImageTk.PhotoImage(self.mailImg)
+        self.mailTab = Button(self.window, width=60, height=60, bg="white")
+        self.mailTab["image"] = self.resizeMailImg
+        self.mailTab.place(x=820, y=262)
+
+        # 챗봇 버튼튼
+        self.chatbotImg = Image.open("img/chatbot.png")
+        self.chatbotImg = self.chatbotImg.resize((60,60),Image.ANTIALIAS)
+        self.resizeChatbotImg = ImageTk.PhotoImage(self.chatbotImg)
+        self.chatbotTab = Button(self.window, width=60, height=60, bg="white")
+        self.chatbotTab["image"] = self.resizeChatbotImg
+        self.chatbotTab.place(x=820, y=430-62)
+
+
+
+# 콤보박스 (시/도), (시/군/구) #########XML
+    def setupCombobox(self):
+        self.combofont = font.Font(self.window, size=14, weight='bold', family='맑은 고딕')
+
+        self.firstCombobox = ttk.Combobox(self.window, width=15,height=5, font = self.combofont)
+        self.firstCombobox['values'] = ('first1', 'first2', 'first3', 'first4', 'first5')
+        self.firstCombobox.set("시/도")
+        self.firstCombobox.place(x=10, y=10)
+        self.firstCombobox.configure(state='readonly')
+
+        self.secondCombobox = ttk.Combobox(self.window, width=15,height=5,font = self.combofont)
+        self.secondCombobox['values'] = ('second1', 'second2', 'second3','second4','second5')
+        self.secondCombobox.set("시/군/구")
+        self.secondCombobox.place(x=220, y=10)
+        self.secondCombobox.configure(state='readonly')
+
+    def search(self):
+        pass
+
+    def undo(self):
+        pass
 
 UI()
 
