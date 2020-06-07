09bb8bdbea5644843b0c2cea805f5f3eaa9904aa
honey1586 <58782737+honey1586@users.noreply.github.com>
2020-06-07 15:09:21 +0900

    gmail base
    내용을 Gmail로 보내기 위해 틀을 만듬.

---
+++ Term project/gmail.py
@@ -0,0 +1,17 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def Gmail():
+    s=smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo()
+    s.starttls()
+    s.login('qorehduf3@gmail.com','jvljgoaecwgljjxe')
+
+    msg = MIMEText("내용: 본문내용 테스트입니다.")
+    msg['Subject'] = '제목: 메일 보내기 테스트입니다.'
+    msg['To'] = 'honey1586@naver.com'
+    s.sendmail('qorehduf3@gmail.com','honey1586@naver.com',msg.as_string())
+
+    s.quit()
+
+Gmail()
