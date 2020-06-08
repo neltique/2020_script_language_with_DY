b579bf378bea341132bb60d83aed55410b342504
neltique <31910313+neltique@users.noreply.github.com>
2020-06-09 00:42:09 +0900

    update AreaBase
    update AreaBase

--- Term project/tour.py
+++ Term project/tour.py
@@ -74,7 +74,56 @@ class AreaCodeXML():
         self.requestUrl()  # 변경된 numOfRows를 적용한 url 요청
         self.updateTree("body/items/item")  # 변경된 numOfRows를 적용한 url tree 만들기
 
+class AreaBasedList():
+    def __init__(self):
+        self.conn = http.client.HTTPConnection("api.visitkorea.or.kr")
+        self.url = ""
+        self.numOfRows = "1"
+        self.areaCode = ""
+        self.sigunguCode = ""
 
+
+    def updateUrl(self):
+        self.url = "/openapi/service/rest/KorService/areaBasedList?serviceKey="+KEY+"&pageNo=1&numOfRows="+self.numOfRows+"&MobileApp=AppTest&MobileOS=ETC&arrange=A&cat1=&contentTypeId=&areaCode="+self.areaCode+"&sigunguCode="+self.sigunguCode+"&cat2=&cat3=&listYN=Y&modifiedtime=&"
+
+    def setAreaCode(self,areaCode):
+        self.areaCode = areaCode
+
+    def setSigunguCode(self, sigunguCode):
+        self.sigunguCode = sigunguCode
+
+    def updateNumOfRows(self):
+        self.numOfRows = self.getNumOfRows()
+
+    def getNumOfRows(self):
+        return self.tree.find("body/totalCount").text
+
+    def requestUrl(self):
+        self.conn.request("GET",self.url)
+        self.req = self.conn.getresponse()
+
+    def updateTree(self,string):
+        self.tree = ET.fromstring(self.req.read().decode('utf-8'))
+        self.get = self.tree.findall(string)
+
+    def makeAreaBasedList(self):    # 시/도 만들기 위해
+        self.updateUrl()        # 첫 url 주소 받기
+        self.requestUrl()       # 요청
+        self.updateTree("body/items/item") #첫 tree만들기 - totalCount를 얻기 위해
+        self.updateNumOfRows()  # toalCount로 numOfRows 변경
+        self.updateUrl()        # 변경된 numOfRows 적용
+        self.requestUrl()       # 변경된 numOfRows를 적용한 url 요청
+        self.updateTree("body/items/item")  # 변경된 numOfRows를 적용한 url tree 만들기
+
+        addrList = []
+        titleList = []
+        contentIdList = []
+        for item in self.get:
+            addrList.append(item.find('addr1').text)
+            titleList.append(item.find('title').text)
+            contentIdList.append(item.find("contentid").text)
+
+        dic = dict(zip(titleList,[addrList, contentIdList]))
 
 
 
