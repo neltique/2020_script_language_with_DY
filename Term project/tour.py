f9557209ece0f4b873761ad27355813e1443e653
neltique <31910313+neltique@users.noreply.github.com>
2020-06-09 18:36:36 +0900

    이제 진짜 새로
    이것부터 완성의 시작이다

--- Term project/tour.py
+++ Term project/tour.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 KEY = "SpWe9UpmMXVZh8MHhnRCpSeVMBO88OXs%2F%2FHIVBSWA3GLBFMhbV9i0WbynUMZ6G66WEUgerpPxoXEVU5DYrQRTg%3D%3D"
 CONN = http.client.HTTPConnection("api.visitkorea.or.kr")
 
+
 class AreaCodeXML():
     def __init__(self):
         self.url = ""
@@ -12,7 +13,7 @@ class AreaCodeXML():
         self.areaCode = ""
 
     def updateUrl(self):
-        self.url = "/openapi/service/rest/KorService/" +self.function+ "?serviceKey="+ KEY +"&numOfRows="+self.numOfRows+"&pageNo=1&MobileOS=ETC&MobileApp=Tour&areaCode="+self.areaCode+"&"
+        self.url = "/openapi/service/rest/KorService/" + self.function + "?serviceKey=" + KEY + "&numOfRows=" + self.numOfRows + "&pageNo=1&MobileOS=ETC&MobileApp=Tour&areaCode=" + self.areaCode + "&"
 
     def updateNumOfRows(self):
         self.numOfRows = self.getNumOfRows()
@@ -32,20 +33,20 @@ class AreaCodeXML():
     def getNumOfRows(self):
         return self.tree.find("body/totalCount").text
 
-    def setNumOfRows(self,num):
+    def setNumOfRows(self, num):
         self.numOfRows = str(num)
 
     def requestUrl(self):
-        CONN.request("GET",self.url)
+        CONN.request("GET", self.url)
         self.req = CONN.getresponse()
 
-    def updateTree(self,string):
+    def updateTree(self, string):
         self.tree = ET.fromstring(self.req.read().decode('utf-8'))
         self.get = self.tree.findall(string)
 
-    def makeAreaCode(self):    # 시/도 만들기 위해
-        self.updateUrl()        # 변경된 numOfRows 적용
-        self.requestUrl()       # 변경된 numOfRows를 적용한 url 요청
+    def makeAreaCode(self):  # 시/도 만들기 위해
+        self.updateUrl()  # 변경된 numOfRows 적용
+        self.requestUrl()  # 변경된 numOfRows를 적용한 url 요청
         self.updateTree("body/items/item")  # 변경된 numOfRows를 적용한 url tree 만들기
 
         lst1 = []
@@ -53,7 +54,7 @@ class AreaCodeXML():
         for item in self.get:
             lst1.append(item.find('name').text)
             lst2.append(item.find('code').text)
-        dic = dict(zip(lst1,lst2))
+        dic = dict(zip(lst1, lst2))
 
         return dic
 
@@ -62,6 +63,7 @@ class AreaCodeXML():
         self.requestUrl()  # 변경된 numOfRows를 적용한 url 요청
         self.updateTree("body/items/item")  # 변경된 numOfRows를 적용한 url tree 만들기
 
+
 class AreaBasedList():
     def __init__(self):
         self.url = ""
@@ -69,11 +71,10 @@ class AreaBasedList():
         self.areaCode = ""
         self.sigunguCode = ""
 
-
     def updateUrl(self):
-        self.url = "/openapi/service/rest/KorService/areaBasedList?serviceKey="+KEY+"&pageNo=1&numOfRows="+self.numOfRows+"&MobileApp=tour&MobileOS=ETC&arrange=A&cat1=&contentTypeId=&areaCode="+self.areaCode+"&sigunguCode="+self.sigunguCode+"&cat2=&cat3=&listYN=Y&modifiedtime=&"
+        self.url = "/openapi/service/rest/KorService/areaBasedList?serviceKey=" + KEY + "&pageNo=1&numOfRows=" + self.numOfRows + "&MobileApp=tour&MobileOS=ETC&arrange=A&cat1=&contentTypeId=&areaCode=" + self.areaCode + "&sigunguCode=" + self.sigunguCode + "&cat2=&cat3=&listYN=Y&modifiedtime=&"
 
-    def setAreaCode(self,areaCode):
+    def setAreaCode(self, areaCode):
         self.areaCode = areaCode
 
     def setSigunguCode(self, sigunguCode):
@@ -86,20 +87,20 @@ class AreaBasedList():
         return self.tree.find("body/totalCount").text
 
     def requestUrl(self):
-        CONN.request("GET",self.url)
+        CONN.request("GET", self.url)
         self.req = CONN.getresponse()
 
-    def updateTree(self,string):
+    def updateTree(self, string):
         self.tree = ET.fromstring(self.req.read().decode('utf-8'))
         self.get = self.tree.findall(string)
 
-    def makeAreaBasedList(self):    # 시/도 만들기 위해
-        self.updateUrl()        # 첫 url 주소 받기
-        self.requestUrl()       # 요청
-        self.updateTree("body/items/item") #첫 tree만들기 - totalCount를 얻기 위해
+    def makeAreaBasedList(self):  # 시/도 만들기 위해
+        self.updateUrl()  # 첫 url 주소 받기
+        self.requestUrl()  # 요청
+        self.updateTree("body/items/item")  # 첫 tree만들기 - totalCount를 얻기 위해
         self.updateNumOfRows()  # toalCount로 numOfRows 변경
-        self.updateUrl()        # 변경된 numOfRows 적용
-        self.requestUrl()       # 변경된 numOfRows를 적용한 url 요청
+        self.updateUrl()  # 변경된 numOfRows 적용
+        self.requestUrl()  # 변경된 numOfRows를 적용한 url 요청
         self.updateTree("body/items/item")  # 변경된 numOfRows를 적용한 url tree 만들기
 
         addrList = []
@@ -119,12 +120,11 @@ class AreaBasedList():
             else:
                 contentIdList.append(item.find("contentid").text)
 
+        return list(zip(titleList, addrList, contentIdList))
 
-
-        return list(zip(titleList,addrList,contentIdList))
 
 def makeDetail(contentId):
-    url = "/openapi/service/rest/KorService/detailCommon?serviceKey="+KEY+"&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=tour&contentId="+contentId+"&contentTypeId=&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&"
+    url = "/openapi/service/rest/KorService/detailCommon?serviceKey=" + KEY + "&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=tour&contentId=" + contentId + "&contentTypeId=&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&"
     CONN.request("GET", url)
     req = CONN.getresponse()
     tree = ET.fromstring(req.read().decode('utf-8'))
@@ -133,7 +133,7 @@ def makeDetail(contentId):
     dic = {}
     for item in get:
         if item.find("addr1") != None:
-            dic["addr1"]= item.find("addr1").text
+            dic["addr1"] = item.find("addr1").text
         if item.find("firstimage") != None:
             dic["firstimage"] = item.find("firstimage").text
         if item.find("mapx") != None:
--- Term project/ui.py
+++ Term project/ui.py
@@ -2,36 +2,29 @@ from tkinter import *
 from tkinter import font
 from tkinter import ttk
 from io import BytesIO
-<<<<<<< HEAD
-import urllib
-import urllib.request
-import folium
-import webbrowser
-from PIL import Image,ImageTk
-=======
 from PIL import ImageTk, Image
 
->>>>>>> a347e857add7a45c259aa0c80843e53490d6f994
 from tour import *
 from gmail import *
 from kakaoMap import *
-
+import folium
+import webbrowser
 import requests
-
 
 WIDTH = 900
 HEIGHT = 450
+
 
 class UI():
     def __init__(self):
         self.window = Tk()
         self.window.title("ReSCH")
-        self.window.geometry(str(WIDTH)+'x'+str(HEIGHT))
-        self.window.resizable(width=False , height = False)
+        self.window.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+        self.window.resizable(width=False, height=False)
         self.window.configure(bg="skyblue")
         self.fontstyle = font.Font(self.window, size=8, weight='bold', family='Consolas')
 
-###############
+        ###############
         self.T = AreaCodeXML()
         self.areaCodeDict1 = self.T.makeAreaCode()
         self.areaCodeDict2 = {}
@@ -42,23 +35,21 @@ class UI():
         self.setupListbox()
         self.setupInfoMapFrame()
 
-
-
-
-########################################################################################################################
+        ########################################################################################################################
         self.window.option_add('*TCombobox*Listbox.font', self.combofont)
         self.window.mainloop()
 
     # 왼쪽 Frame 리스트 박스 준비
     def setupListbox(self):
-        frame = Frame(self.window,width=320,height=385,bg="gray")
-        frame.place(x=10,y=50)
+        frame = Frame(self.window, width=320, height=385, bg="gray")
+        frame.place(x=10, y=50)
 
         scrollbar = Scrollbar(frame)
-        scrollbar.pack(side=RIGHT,fill=Y)
+        scrollbar.pack(side=RIGHT, fill=Y)
 
         self.adressfont = font.Font(frame, size=11, weight='bold', family='Consolas')
-        self.adressList = Listbox(frame,selectmode ='browse',width=38,height = 20,font=self.adressfont,yscrollcommand = scrollbar.set, activestyle='none')
+        self.adressList = Listbox(frame, selectmode='browse', width=38, height=20, font=self.adressfont,
+                                  yscrollcommand=scrollbar.set, activestyle='none')
         scrollbar["command"] = self.adressList.yview
         self.adressList.pack(side=LEFT)
 
@@ -67,16 +58,12 @@ class UI():
     # 리스트에서 선택
     def selectList(self, *args):
         contentid = str(self.searchList[self.adressList.curselection()[0]][2])
-<<<<<<< HEAD
+        self.distroyInfoLabels()
         #print(str(contentid))
         self.mapx = float(makeDetail(contentid)["mapx"])
         self.mapy = float(makeDetail(contentid)["mapy"])
 
-=======
-        self.distroyInfoLabels()
         self.pressedInfo(makeDetail(contentid))
->>>>>>> a347e857add7a45c259aa0c80843e53490d6f994
-
 
     # 오른쪽 Frame 정보, 지도 그릴 Frame
     def setupInfoMapFrame(self):
@@ -98,7 +85,6 @@ class UI():
     def afterCanvasScroll(self, event):
         self.secondCanvas.configure(scrollregion=self.secondCanvas.bbox("all"), width=450, height=380, bg="white")
 
-
     def distroyInfoLabels(self):
         for x in self.secondCanvas.find_all():
             self.secondCanvas.delete(x)
@@ -107,12 +93,13 @@ class UI():
     def setupButton(self):
         # 검색 버튼
         self.searchfont = font.Font(self.window, size=11, weight='bold', family='Consolas')
-        self.searchButton = Button(text="검색", width=5, height=1, font=self.searchfont, bg="white", fg="black",command=self.search)
+        self.searchButton = Button(text="검색", width=5, height=1, font=self.searchfont, bg="white", fg="black",
+                                   command=self.search)
         self.searchButton.place(x=430, y=10)
 
         # 정보 버튼
         self.infoImg = Image.open("img/info.png")
-        self.infoImg = self.infoImg.resize((60,60), Image.ANTIALIAS)
+        self.infoImg = self.infoImg.resize((60, 60), Image.ANTIALIAS)
         self.resizeInfoImg = ImageTk.PhotoImage(self.infoImg)
         self.infoTab = Button(self.window, width=60, height=60, bg="white", command=self.pressedInfo)
         self.infoTab["image"] = self.resizeInfoImg
@@ -120,54 +107,49 @@ class UI():
 
         # 지도 버튼
         self.mapImg = Image.open("img/map.png")
-        self.mapImg = self.mapImg.resize((60,60), Image.ANTIALIAS)
+        self.mapImg = self.mapImg.resize((60, 60), Image.ANTIALIAS)
         self.resizeMapImg = ImageTk.PhotoImage(self.mapImg)
-        self.mapTab = Button(self.window, width=60, height=60, bg="white",command = self.pressedMap)
+        self.mapTab = Button(self.window, width=60, height=60, bg="white", command=self.pressedMap)
         self.mapTab["image"] = self.resizeMapImg
         self.mapTab.place(x=820, y=156)
 
         # 메일 보내기 버튼
         self.mailImg = Image.open("img/mail.png")
-        self.mailImg = self.mailImg.resize((60,60), Image.ANTIALIAS)
+        self.mailImg = self.mailImg.resize((60, 60), Image.ANTIALIAS)
         self.resizeMailImg = ImageTk.PhotoImage(self.mailImg)
-        self.mailTab = Button(self.window, width=60, height=60, bg="white",command = Gmail())
+        self.mailTab = Button(self.window, width=60, height=60, bg="white", command=Gmail())
         self.mailTab["image"] = self.resizeMailImg
         self.mailTab.place(x=820, y=262)
 
         # 챗봇 버튼튼
         self.chatbotImg = Image.open("img/chatbot.png")
-        self.chatbotImg = self.chatbotImg.resize((60,60),Image.ANTIALIAS)
+        self.chatbotImg = self.chatbotImg.resize((60, 60), Image.ANTIALIAS)
         self.resizeChatbotImg = ImageTk.PhotoImage(self.chatbotImg)
         self.chatbotTab = Button(self.window, width=60, height=60, bg="white")
         self.chatbotTab["image"] = self.resizeChatbotImg
-        self.chatbotTab.place(x=820, y=430-62)
-
+        self.chatbotTab.place(x=820, y=430 - 62)
 
     # 콤보박스 (시/도), (시/군/구)
     def setupCombobox(self):
         self.combofont = font.Font(self.window, size=14, weight='bold', family='맑은 고딕')
 
-        self.firstCombobox = ttk.Combobox(self.window, width=15,height=5, font=self.combofont)
-
+        self.firstCombobox = ttk.Combobox(self.window, width=15, height=5, font=self.combofont)
 
         # 시/도 검색
-        lst = [] #
+        lst = []  #
         for value in self.areaCodeDict1.keys():
             lst.append(value)
         self.firstCombobox['values'] = tuple(lst)
 
-
         self.firstCombobox.set("시/도")
         self.firstCombobox.place(x=10, y=10)
         self.firstCombobox.configure(state='readonly')
-
 
         self.secondCombobox = ttk.Combobox(self.window, width=15, height=5, font=self.combofont)
         self.secondCombobox['values'] = ('')
         self.secondCombobox.set("시/군/구")
         self.secondCombobox.place(x=220, y=10)
         self.secondCombobox.configure(state='disabled')
-
 
         self.firstCombobox.bind("<<ComboboxSelected>>", self.firstComb_selected)
 
@@ -194,10 +176,10 @@ class UI():
         i = 0
         for l in self.searchList:
             self.adressList.insert(i, l[0])
-            i+=1
+            i += 1
 
     def pressedMap(self):
-<<<<<<< HEAD
+        self.distroyInfoLabels()
 
         m = folium.Map(location=[self.mapy, self.mapx],
                        tiles="OpenStreetMap", zoom_start=16)
@@ -207,10 +189,6 @@ class UI():
 
         m.save("map.html")
         webbrowser.open_new('map.html')
-
-=======
-        self.distroyInfoLabels()
-
 
     def pressedInfo(self, d):
         print("dictionary")
@@ -276,7 +254,8 @@ class UI():
             i += 2
 
         if 'zipcode' in dictInfo:
-            Label(self.secondFrame, text="우편번호 - " + dictInfo['zipcode'] + "\n", bg="white", justify='left').grid(row=i, column=1)
+            Label(self.secondFrame, text="우편번호 - " + dictInfo['zipcode'] + "\n", bg="white", justify='left').grid(row=i,
+                                                                                                                  column=1)
             i += 2
 
         if 'overview' in dictInfo:
@@ -299,8 +278,6 @@ class UI():
                 str1 += str[:c] + "\n"
 
             Label(self.secondFrame, text=str1, bg="white", justify='left').grid(row=i, column=1)
->>>>>>> a347e857add7a45c259aa0c80843e53490d6f994
 
 
 UI()
-
